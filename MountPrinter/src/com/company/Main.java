017f2b52c74917c5a28a8e4952de51b4abe5c11d
nabacarry <56451777+nabacarry@users.noreply.github.com>
2019-10-15 16:26:22 +0300

    test

---
+++ MountPrinter/src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int monthNumber = scanner.nextInt();
+        String monthString = "";
+        switch (monthNumber)
+        {
+            case 1:
+                monthString = "January";
+                break;
+            case 2:
+                monthString = "February";
+                break;
+            case 3:
+                monthString = "March";
+                break;
+            case 4:
+                monthString = "April";
+                break;
+            case 5:
+                monthString = "May";
+                break;
+            case 6:
+                monthString = "June";
+                break;
+            case 7:
+                monthString = "July";
+                break;
+            case 8:
+                monthString = "August";
+                break;
+            case 9:
+                monthString = "September";
+                break;
+            case 10:
+                monthString = "October";
+                break;
+            case 11:
+                monthString = "November";
+                break;
+            case 12:
+                monthString = "December";
+                break;
+        }
+
+        String textMount = String.format("Mount is: %s", monthString);
+        System.out.println(textMount);
+
+    }
+}
