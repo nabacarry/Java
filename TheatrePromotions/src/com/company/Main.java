e04ad9fed5dd3af059f9e6ec104d60f6951b5162
nabacarry <56451777+nabacarry@users.noreply.github.com>
2019-10-15 20:49:46 +0300

    up

--- TheatrePromotions/src/com/company/Main.java
+++ TheatrePromotions/src/com/company/Main.java
@@ -1,8 +1,77 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Day type:");
+        String dayType = scanner.nextLine().toLowerCase();
+        if (!dayType.equals("weekday")  && !dayType.equals("weekend") && !dayType.equals("holiday"))
+        {
+            System.out.println("Invalid day type");
+            System.exit(0);
+        }
+        System.out.println("Ages:");
+        int age = scanner.nextInt();
+        int price = 0;
+
+
+        if (dayType.equals("weekday"))
+        {
+            if ((age >= 0 && age <= 18) || (age > 64 && age <= 122))
+            {
+                price = 12;
+            }else if(age > 18 && age <= 64)
+            {
+                price = 18;
+            }else
+            {
+                System.out.println("Error");
+                System.exit(0);
+            }
+        }
+
+        if (dayType.equals("weekend"))
+        {
+            if((age >= 0 && age <= 18) || (age > 64 && age <= 122))
+            {
+                price = 15;
+            }else if (age > 18 && age < 64)
+            {
+                price = 20;
+            }else
+            {
+                System.out.println("Error");
+                System.exit(0);
+            }
+        }
+        if(dayType.equals("holiday"))
+        {
+            if (age >= 0 && age <= 18)
+            {
+                price = 5;
+            }else if (age > 18 && age <= 64)
+            {
+                price = 12;
+            }else if (age > 64 && age <= 122)
+            {
+                price = 10;
+            }else
+            {
+                System.out.println("Error");
+                System.exit(0);
+            }
+        }
+
+        if(price != 0)
+        {
+            String textDayPrice = String.format("For %s price is: %d$", dayType, price);
+            System.out.println(textDayPrice);
+        }
+
     }
 }
