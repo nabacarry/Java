3eac2261e0e0f7e8610163a601d684a1ec26c57c
nabacarry <56451777+nabacarry@users.noreply.github.com>
2019-10-15 19:35:08 +0300

    up

--- ForeignLanguages/src/com/company/Main.java
+++ ForeignLanguages/src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        String countryLang = scanner.nextLine();
+
+        switch (countryLang)
+        {
+            case "USA":
+            case "England":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("Unknown");
+        }
+
     }
 }
