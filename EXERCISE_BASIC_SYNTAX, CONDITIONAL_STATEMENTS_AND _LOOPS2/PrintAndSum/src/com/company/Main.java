b4f98286b2ccf1fbdde0d53f80264eae872ab34b
nabacarry <56451777+nabacarry@users.noreply.github.com>
2019-10-17 13:22:55 +0300

    up

--- EXERCISE_BASIC_SYNTAX, CONDITIONAL_STATEMENTS_AND _LOOPS2/PrintAndSum/src/com/company/Main.java
+++ EXERCISE_BASIC_SYNTAX, CONDITIONAL_STATEMENTS_AND _LOOPS2/PrintAndSum/src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int lastNumber = Integer.parseInt(scanner.nextLine());
+        int sumNumber = 0;
+
+        for (int i = firstNumber; i <= lastNumber; i++)
+        {
+            System.out.print(i + " ");
+            sumNumber += i;
+        }
+        System.out.printf("%nSum: %d", sumNumber);
     }
 }
