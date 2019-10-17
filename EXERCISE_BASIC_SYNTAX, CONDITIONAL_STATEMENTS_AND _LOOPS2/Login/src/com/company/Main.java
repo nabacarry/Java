37b078035c7faacd379ee3d0c16d432518ecbdb9
nabacarry <56451777+nabacarry@users.noreply.github.com>
2019-10-17 18:49:42 +0300

    up

--- EXERCISE_BASIC_SYNTAX, CONDITIONAL_STATEMENTS_AND _LOOPS2/Login/src/com/company/Main.java
+++ EXERCISE_BASIC_SYNTAX, CONDITIONAL_STATEMENTS_AND _LOOPS2/Login/src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        String username = scanner.nextLine();
+        String password = "";
+
+        for (int i = username.length() - 1; i >= 0; i--)
+        {
+            password += username.charAt(i);
+        }
+
+        String inputPassword = scanner.nextLine();
+        int counter = 1;
+        while (!password.equals(inputPassword) && counter < 4)
+        {
+            System.out.println("Incorrect password.Try again");
+            inputPassword = scanner.nextLine();
+            counter++;
+        }
+        if (password.equals(inputPassword))
+        {
+            System.out.printf("User %s logged in.",username);
+        }else
+        {
+            System.out.printf("User %s blocked!",username);
+        }
     }
 }
